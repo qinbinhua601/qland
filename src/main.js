c2f66ee48987abef845df47a055227b4edbf76f1
alexqin@intv.com.cn <alexqin@intv.com.cn>
2017-01-05 22:34:21 +0800

    using the require echarts instead of npm install

--- src/main.js
+++ src/main.js
@@ -4,13 +4,7 @@ import router from './router/'
 import _ from 'lodash'
 import iSlider from 'islider.js'
 
-// 引入 ECharts 主模块
-let echarts = require('echarts/lib/echarts')
-// 引入柱状图
-require('echarts/lib/chart/line')
-// 引入提示框和标题组件
-require('echarts/lib/component/tooltip')
-require('echarts/lib/component/title')
+let echarts = require('./plugins/echarts.common.min.js')
 
 if(window !== 'undefined') {
   window.echarts = echarts
